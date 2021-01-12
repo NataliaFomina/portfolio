7b6c2f07272c151562168f02329773536f78d670
Natalia Fomina <fominatasha@gmail.com>
2021-01-12 09:44:08 +0100

    update Experience component

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -6,27 +6,112 @@ function Experience() {
       <div className="experience-section">
         <section className="container">
           <h2 className="title">My Experience</h2>
-          <div className="wrapper">
-            <div className="item first">
-              <p className="year">2018-2019</p>
-              <p className="profession">Web Master</p>
 
-              <p className="location">Maskom - Moscow, Russia</p>
-              <p><i className="fal fa-link"></i><a href="https://maskomzoloto.ru/">https://maskomzoloto.ru/</a></p>
-              <p className="obligation">
-                <ul className="list">Development of UI:
-                  <li>Creating elements (sliders, calculators)</li>
-                  <li>Layouts for landing pages</li>
-                  <li>Responsive layout / for mobile devices</li>
-                  <li>SEO optimization</li>
-                  <li>Support for an existing site</li>
-                </ul>
+          <div className="items">
+            <div className="item">
+              <p className="year">2020-2021</p>
+              <p className="job">Web Developer & Web Designer, Founder</p>
+              <p>
+                <i className="fal fa-map-pin"></i>Snippet Stock - Frankfurt, Germany
               </p>
-
+              <ul>Development of UI:
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+                <li>5</li>
+              </ul>
             </div>
-            <div className="item second">ex 2</div>
-            <div className="item third">ex 3</div>
-            <div className="item fourth">ex 4</div>
+
+            <div className="item">
+              <p className="year">2019-2020</p>
+              <p className="job">Frontend Developer</p>
+              <p>
+                <i className="fal fa-map-pin"></i>Mygento - Saint Petersburg, Russia
+              </p>
+              <p>
+                <i className="fal fa-link"></i><a href="https://www.mygento.ru">https://www.mygento.ru</a>
+              </p>
+              <ul>Development of UI:
+                <li>work with client manager and backend team</li>
+                <li>develop frontend components</li>
+                <li>creating animation, banners, sliders, blogs</li>
+                <li>refactor code</li>
+                <li>Support for an existing websites L'Oréal, MyGlo</li>
+              </ul>
+            </div>
+
+            <div className="item">
+              <p className="year">2019-2020</p>
+              <p className="job">Web Developer</p>
+              <p>
+                <i className="fal fa-map-pin"></i>AvtoHarakter - Saint Petersburg, Russia
+              </p>
+              <p>
+                <i className="fal fa-link"></i><a href=""></a>
+              </p>
+              <ul>Development of UI:
+                <li>work with client manager and backend team</li>
+                <li>develop frontend components</li>
+                <li>creating animation, banners, sliders, blogs</li>
+                <li>refactor code</li>
+                <li>Support for an existing websites L'Oréal, MyGlo</li>
+              </ul>
+            </div>
+
+            <div className="item">
+              <p className="year">2018-2019</p>
+              <p className="job">Web Developer</p>
+              <p>
+                <i className="fal fa-map-pin"></i>Procent - Saint Petersburg, Russia
+              </p>
+              <p>
+                <i className="fal fa-link"></i><a href="http://процентденьги.рф">http://процентденьги.рф</a>
+              </p>
+              <ul>Development of UI:
+                <li>Creating elements (sliders, calculators)</li>
+                <li>Design layouts for landing pages</li>
+                <li>Responsive layout / for mobile devices</li>
+                <li>SEO optimization</li>
+                <li>Support for an existing site</li>
+              </ul>
+            </div>
+
+            <div className="item">
+              <p className="year">2018-2019</p>
+              <p className="job">Web Developer</p>
+              <p>
+                <i className="fal fa-map-pin"></i>Granit - Saint Petersburg, Russia
+              </p>
+              <p>
+                <i className="fal fa-link"></i><a href="http://granitbrz.ru">http://granitbrz.ru</a>
+              </p>
+              <ul>Development of UI:
+                <li>Creating elements (sliders, calculators)</li>
+                <li>Design layouts for landing pages</li>
+                <li>Responsive layout / for mobile devices</li>
+                <li>SEO optimization</li>
+                <li>Support for an existing site</li>
+              </ul>
+            </div>
+
+            <div className="item">
+              <p className="year">2017-2018</p>
+              <p className="job">Web Master</p>
+              <p>
+                <i className="fal fa-map-pin"></i>Maskom - Moscow, Russia
+              </p>
+              <p>
+                <i className="fal fa-link"></i><a href="https://maskomzoloto.ru">https://maskomzoloto.ru</a>
+              </p>
+              <ul>Development of UI:
+                <li>Creating elements (sliders, calculators)</li>
+                <li>Design layouts for landing pages</li>
+                <li>Responsive layout / for mobile devices</li>
+                <li>SEO optimization</li>
+                <li>Support for an existing site</li>
+              </ul>
+            </div>
           </div>
         </section>
       </div>
