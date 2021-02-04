990420647cd8140f4b821df174cd53ab32014a31
Natalia Fomina <fominatasha@gmail.com>
2021-02-04 17:22:32 +0100

    update About, Contact component

--- src/About/About.js
+++ src/About/About.js
@@ -6,13 +6,13 @@ function About() {
     <div className="about-section">
       <section className="about-container container">
         <h1 className="about-name">NATALIA FOMINA</h1>
-        <p className="about-profession">Web Developer & Web Designer</p>
+        <p className="about-profession">Web Developer & Art Director</p>
         <p className="about-text"><i className="fal fa-heart"></i>I love design, web development and art.</p>
         <p className="about-text"><i className="fal fa-map-pin"></i>I'm from Saint Petersburg, and I live in Frankfurt.</p>
         <p className="about-text"><i className="fal fa-globe-americas"></i>I speak fluent English, Italian, Russian. I'm actively learning German.</p>
         <div className="about-btn-wrap">
-          <button className="about-btn">My works</button>
-          <button className="about-btn">Contact me</button>
+          <button className="about-btn">Download my CV <i className="fal fa-arrow-to-bottom"></i></button>
+          {/*<button className="about-btn">Contact me</button>*/}
         </div>
       </section>
     </div>
--- src/Contacts/Contacts.js
+++ src/Contacts/Contacts.js
@@ -1,8 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Contacts() {
-  return(
-      <div>Contacts</div>
+  const  [status, setStatus ] = useState();
+  function submitForm(ev) {
+    console.log(ev);
+
+    ev.preventDefault();
+    const form = ev.target;
+    const data = new FormData(form);
+    const xhr = new XMLHttpRequest();
+    xhr.open(form.method, form.action);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        form.reset();
+        setStatus("SUCCESS");
+      } else {
+        setStatus("ERROR");
+      }
+    };
+    xhr.send(data);
+  }
+
+  return (
+    <div className="contact-section container">
+
+
+      <form
+        onSubmit={submitForm}
+        action="https://formspree.io/f/xpzonjaz"
+        method="POST"
+        className="contact-form"
+      >
+
+        <label>Email:</label>
+        <input type="email" name="email" placeholder="Email"/>
+        <label>Message:</label>
+        <textarea name="message" id="">
+
+        </textarea>
+        {status === "SUCCESS" ? <p>Thanks!</p> : <button>Submit</button>}
+        {status === "ERROR" && <p>Ooops! There was an error.</p>}
+      </form>
+    </div>
   )
 }
 
