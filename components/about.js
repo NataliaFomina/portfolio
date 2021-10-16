94ba38b097b47741928591371aa6e2b01244f11d
Natalia Fomina <fominatasha@gmail.com>
2021-10-16 12:49:10 +0200

    Change text about

--- components/about.js
+++ components/about.js
@@ -9,7 +9,7 @@ function About() {
         <h1 className="about-name">{t('NATALIA FOMINA')}</h1>
         <p className="about-profession">{t('Front-end Developer & Designer')}</p>
         <p className="about-text"><i className="fal fa-heart"/>{t('I love web development, design and art.')}</p>
-        <p className="about-text"><i className="fal fa-map-pin"/>{t('I\'m from Saint Petersburg, Russia.')}</p>
+        <p className="about-text"><i className="fal fa-map-pin"/>{t('I live in Munich, Germany.')}</p>
         <p className="about-text"><i className="fal fa-globe-americas"/>{t('I speak fluent English, Italian, Russian. I\'m beginner in German.')}</p>
         <div className="about-btn-wrap">
           <a href="https://natalia-portfolio-api.herokuapp.com/" rel="noreferrer" target="_blank" className="about-btn">{t('Download my CV')} <i
