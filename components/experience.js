35b138790a22bbfe8d8cd46598cfff31f5a6b4e1
Natalia Fomina <fominatasha@gmail.com>
2022-05-17 22:13:06 +0200

    Update icons and fonts

--- components/experience.js
+++ components/experience.js
@@ -1,171 +1,322 @@
-import React from 'react';
-import {useTranslation} from "react-i18next";
+import React from "react";
+import { useTranslation } from "react-i18next";
 
 function Experience() {
-  const {t} = useTranslation();
+  const { t } = useTranslation();
   return (
     <div className="experience-section">
       <section className="experience-container container">
-        <h2 className="title">{t('Work Experience')}</h2>
+        <h2 className="title">{t("Work Experience")}</h2>
 
         <div className="items">
-
           <div className="item">
-            <p className="year">{t('Feb 2020 - Nov 2020')}</p>
-            <p className="job">{t('Frontend Developer')}</p>
+            <p className="year">{t("Nov 2021 - Current")}</p>
+            <p className="job">{t("Frontend Developer")}</p>
             <p>
-              <i className="fal fa-map-pin"/>{t('Mygento - Saint Petersburg, Russia')}
+              <i className="fal fa-map-pin" />
+              {t("Genesis Cloud - Munich, Germany")}
             </p>
             <p>
-              <i className="fal fa-link"/><a href="https://www.mygento.ru" target="_blank"
-                                             rel="noreferrer">https://www.mygento.ru</a>
+              <i className="fal fa-link" />
+              <a
+                href="https://www.genesiscloud.com"
+                target="_blank"
+                rel="noreferrer"
+              >
+                https://www.genesiscloud.com
+              </a>
             </p>
             <ul>
-              <li>{t('Development of React based component, featuring responsive design that supports mobile and tablet')}
+              <li>{t("Define frontend architecture of dashboard")}</li>
+              <li>{t("Create task for developers and code review")}</li>
+              <li>
+                {t(
+                  "Refactor old version of dashboard with new architecture, using  functional components, Redux toolkit, Chakra UI, SWR."
+                )}
               </li>
-              <li>{t('Development of frontend elements: sliders, menus, blog, subscription, product card, cart animation, and more')}
+              <li>
+                {t(
+                  "Take part in conversation, give technical opinion about implementation new features"
+                )}
               </li>
-              <li>{t('Code refactoring')}</li>
-              <li>{t('Integration testing')}</li>
-              <li>{t('Collaborating with backend developers')}</li>
-              <li>{t('Working directly with clients; taking part in discussions, setting up tasks and goals')}</li>
-              <li>{t('Content management & administration')}</li>
-              <li>{t('Taking part in the migration of Magento 1 online shop to Magento 2')}</li>
-              <li>{t('Support and maintenance of existing products: L\'Oréal, MyGlo')}</li>
+              <li>
+                {t(
+                  "Development of React based component, featuring responsive design that supports mobile and tablet."
+                )}
+              </li>
+              <li>{t("Support and maintenance of website and dashboard")}</li>
+            </ul>
+          </div>
+
+          <div className="item">
+            <p className="year">{t("Feb 2020 - Nov 2020")}</p>
+            <p className="job">{t("Frontend Developer")}</p>
+            <p>
+              <i className="fal fa-map-pin" />
+              {t("Mygento - Saint Petersburg, Russia")}
+            </p>
+            <p>
+              <i className="fal fa-link" />
+              <a href="https://www.mygento.ru" target="_blank" rel="noreferrer">
+                https://www.mygento.ru
+              </a>
+            </p>
+            <ul>
+              <li>
+                {t(
+                  "Development of React based component, featuring responsive design that supports mobile and tablet"
+                )}
+              </li>
+              <li>
+                {t(
+                  "Development of frontend elements: sliders, menus, blog, subscription, product card, cart animation, and more"
+                )}
+              </li>
+              <li>{t("Code refactoring")}</li>
+              <li>{t("Integration testing")}</li>
+              <li>{t("Collaborating with backend developers")}</li>
+              <li>
+                {t(
+                  "Working directly with clients; taking part in discussions, setting up tasks and goals"
+                )}
+              </li>
+              <li>{t("Content management & administration")}</li>
+              <li>
+                {t(
+                  "Taking part in the migration of Magento 1 online shop to Magento 2"
+                )}
+              </li>
+              <li>
+                {t(
+                  "Support and maintenance of existing products: L'Oréal, MyGlo"
+                )}
+              </li>
             </ul>
           </div>
 
           <div className="item">
             <p className="year">2019</p>
-            <p className="job">{t('Web Developer (Freelancer)')}</p>
+            <p className="job">{t("Web Developer (Freelancer)")}</p>
             <p>
-              <i className="fal fa-map-pin"/>{t('Autoharakter - Saint Petersburg, Russia')}
+              <i className="fal fa-map-pin" />
+              {t("Autoharakter - Saint Petersburg, Russia")}
             </p>
             <p>
-              <i className="fal fa-link"/><a href="https://autoharakter.ru" target="_blank"
-                                             rel="noreferrer">https://autoharakter.ru</a>
+              <i className="fal fa-link" />
+              <a
+                href="https://autoharakter.ru"
+                target="_blank"
+                rel="noreferrer"
+              >
+                https://autoharakter.ru
+              </a>
             </p>
             <ul>
-              <li>{t('Development of a responsive web frontend for desktop, mobile and tablet')}</li>
-              <li>{t('Design of UI & UX')}</li>
-              <li>{t('Deployment and hosting setup')}</li>
-              <li>{t('Defining a sitemap')}</li>
-              <li>{t('Working directly with clients; discussing and setting goals')}</li>
-              <li>{t('Working with article writers')}</li>
-              <li>{t('Optimizing SEO for Google and Yandex searching engines')}</li>
-              <li>{t('Maintenance of existing products')}</li>
+              <li>
+                {t(
+                  "Development of a responsive web frontend for desktop, mobile and tablet"
+                )}
+              </li>
+              <li>{t("Design of UI & UX")}</li>
+              <li>{t("Deployment and hosting setup")}</li>
+              <li>{t("Defining a sitemap")}</li>
+              <li>
+                {t(
+                  "Working directly with clients; discussing and setting goals"
+                )}
+              </li>
+              <li>{t("Working with article writers")}</li>
+              <li>
+                {t("Optimizing SEO for Google and Yandex searching engines")}
+              </li>
+              <li>{t("Maintenance of existing products")}</li>
             </ul>
           </div>
 
           <div className="item">
             <p className="year">2019</p>
-            <p className="job">{t('Web Developer (Freelancer)')}</p>
+            <p className="job">{t("Web Developer (Freelancer)")}</p>
             <p>
-              <i className="fal fa-map-pin"/>{t('Procent - Saint Petersburg, Russia')}
+              <i className="fal fa-map-pin" />
+              {t("Procent - Saint Petersburg, Russia")}
             </p>
             <p>
-              <i className="fal fa-link"/><a href="http://процентденьги.рф" target="_blank"
-                                             rel="noreferrer">http://процентденьги.рф</a>
+              <i className="fal fa-link" />
+              <a
+                href="http://процентденьги.рф"
+                target="_blank"
+                rel="noreferrer"
+              >
+                http://процентденьги.рф
+              </a>
             </p>
             <ul>
-              <li>{t('Development of a responsive web frontend for desktop, mobile and tablet')}</li>
-              <li>{t('Design of UI & UX')}</li>
-              <li>{t('Deployment and hosting setup')}</li>
-              <li>{t('Defining a sitemap')}</li>
-              <li>{t('Working directly with clients; discussing and setting goals')}</li>
-              <li>{t('Working with article writers')}</li>
-              <li>{t('Optimizing SEO for Google and Yandex searching engines')}</li>
-              <li>{t('Maintenance of existing products')}</li>
+              <li>
+                {t(
+                  "Development of a responsive web frontend for desktop, mobile and tablet"
+                )}
+              </li>
+              <li>{t("Design of UI & UX")}</li>
+              <li>{t("Deployment and hosting setup")}</li>
+              <li>{t("Defining a sitemap")}</li>
+              <li>
+                {t(
+                  "Working directly with clients; discussing and setting goals"
+                )}
+              </li>
+              <li>{t("Working with article writers")}</li>
+              <li>
+                {t("Optimizing SEO for Google and Yandex searching engines")}
+              </li>
+              <li>{t("Maintenance of existing products")}</li>
             </ul>
           </div>
 
           <div className="item">
             <p className="year">2018</p>
-            <p className="job">{t('Web Developer (Freelancer)')}</p>
+            <p className="job">{t("Web Developer (Freelancer)")}</p>
             <p>
-              <i className="fal fa-map-pin"/>{t('Granit - Saint Petersburg, Russia')}
+              <i className="fal fa-map-pin" />
+              {t("Granit - Saint Petersburg, Russia")}
             </p>
             <p>
-              <i className="fal fa-link"/><a href="http://granitbrz.ru" target="_blank"
-                                             rel="noreferrer">http://granitbrz.ru</a>
+              <i className="fal fa-link" />
+              <a href="http://granitbrz.ru" target="_blank" rel="noreferrer">
+                http://granitbrz.ru
+              </a>
             </p>
             <ul>
-              <li>{t('Development of a responsive web frontend for desktop, mobile and tablet')}</li>
-              <li>{t('Design of UI & UX')}</li>
-              <li>{t('Deployment and hosting setup')}</li>
-              <li>{t('Defining a sitemap')}</li>
-              <li>{t('Working directly with clients; discussing and setting goals')}</li>
-              <li>{t('Working with article writers')}</li>
-              <li>{t('Optimizing SEO for Google and Yandex searching engines')}</li>
-              <li>{t('Maintenance of existing products')}</li>
+              <li>
+                {t(
+                  "Development of a responsive web frontend for desktop, mobile and tablet"
+                )}
+              </li>
+              <li>{t("Design of UI & UX")}</li>
+              <li>{t("Deployment and hosting setup")}</li>
+              <li>{t("Defining a sitemap")}</li>
+              <li>
+                {t(
+                  "Working directly with clients; discussing and setting goals"
+                )}
+              </li>
+              <li>{t("Working with article writers")}</li>
+              <li>
+                {t("Optimizing SEO for Google and Yandex searching engines")}
+              </li>
+              <li>{t("Maintenance of existing products")}</li>
             </ul>
           </div>
 
           <div className="item">
             <p className="year">2018</p>
-            <p className="job">{t('Web Developer (Freelancer)')}</p>
+            <p className="job">{t("Web Developer (Freelancer)")}</p>
             <p>
-              <i className="fal fa-map-pin"/>{t('Enduro - Saint Petersburg, Russia')}
+              <i className="fal fa-map-pin" />
+              {t("Enduro - Saint Petersburg, Russia")}
             </p>
             <p>
-              <i className="fal fa-link"/><a href="http://crimeatrophy.ru" target="_blank"
-                                             rel="noreferrer">http://crimeatrophy.ru</a>
+              <i className="fal fa-link" />
+              <a href="http://crimeatrophy.ru" target="_blank" rel="noreferrer">
+                http://crimeatrophy.ru
+              </a>
             </p>
             <ul>
-              <li>{t('Development of a responsive web frontend for desktop, mobile and tablet')}</li>
-              <li>{t('Design of UI & UX')}</li>
-              <li>{t('Deployment and hosting setup')}</li>
-              <li>{t('Defining a sitemap')}</li>
-              <li>{t('Working directly with clients; discussing and setting goals')}</li>
-              <li>{t('Working with article writers')}</li>
-              <li>{t('Optimizing SEO for Google and Yandex searching engines')}</li>
-              <li>{t('Maintenance of existing products')}</li>
+              <li>
+                {t(
+                  "Development of a responsive web frontend for desktop, mobile and tablet"
+                )}
+              </li>
+              <li>{t("Design of UI & UX")}</li>
+              <li>{t("Deployment and hosting setup")}</li>
+              <li>{t("Defining a sitemap")}</li>
+              <li>
+                {t(
+                  "Working directly with clients; discussing and setting goals"
+                )}
+              </li>
+              <li>{t("Working with article writers")}</li>
+              <li>
+                {t("Optimizing SEO for Google and Yandex searching engines")}
+              </li>
+              <li>{t("Maintenance of existing products")}</li>
             </ul>
           </div>
 
           <div className="item">
             <p className="year">2017</p>
-            <p className="job">{t('Web Master (Freelancer)')}</p>
+            <p className="job">{t("Web Master (Freelancer)")}</p>
             <p>
-              <i className="fal fa-map-pin"/>{t('Maskom - Moscow, Russia')}
+              <i className="fal fa-map-pin" />
+              {t("Maskom - Moscow, Russia")}
             </p>
             <p>
-              <i className="fal fa-link"/><a href="https://maskomzoloto.ru" target="_blank"
-                                             rel="noreferrer">https://maskomzoloto.ru</a>
+              <i className="fal fa-link" />
+              <a
+                href="https://maskomzoloto.ru"
+                target="_blank"
+                rel="noreferrer"
+              >
+                https://maskomzoloto.ru
+              </a>
             </p>
-            <ul>{t('Development of UI:')}
-              <li>{t('Creating elements (sliders, calculators)')}</li>
-              <li>{t('Design layouts for landing pages')}</li>
-              <li>{t('Responsive layout / for mobile devices')}</li>
-              <li>{t('SEO optimization')}</li>
-              <li>{t('Support for an existing site')}</li>
+            <ul>
+              {t("Development of UI:")}
+              <li>{t("Creating elements (sliders, calculators)")}</li>
+              <li>{t("Design layouts for landing pages")}</li>
+              <li>{t("Responsive layout / for mobile devices")}</li>
+              <li>{t("SEO optimization")}</li>
+              <li>{t("Support for an existing site")}</li>
             </ul>
           </div>
 
           <div className="item">
-            <p className="year">{t('Sep 2015 - Nov 2016')}</p>
-            <p className="job">{t('Art Director & Stylist')}</p>
+            <p className="year">{t("Sep 2015 - Nov 2016")}</p>
+            <p className="job">{t("Art Director & Stylist")}</p>
             <p>
-              <i className="fal fa-map-pin"/>{t('LYYK & Lomonosova 22 - Saint Petersburg, Russia')}
+              <i className="fal fa-map-pin" />
+              {t("LYYK & Lomonosova 22 - Saint Petersburg, Russia")}
             </p>
             <p>
-              <i className="fal fa-link"/><a href="https://www.instagram.com/lomonosova22" target="_blank"
-                                             rel="noreferrer">https://lomonosova22</a>
+              <i className="fal fa-link" />
+              <a
+                href="https://www.instagram.com/lomonosova22"
+                target="_blank"
+                rel="noreferrer"
+              >
+                https://lomonosova22
+              </a>
             </p>
             <ul>
-              <li>{t('Creating brand image & style for two shops')}</li>
-              <li>{t('Ideas for photo shootings and participation in it: choosing location, model, photographer, make up, style and coordinate all process')}
+              <li>{t("Creating brand image & style for two shops")}</li>
+              <li>
+                {t(
+                  "Ideas for photo shootings and participation in it: choosing location, model, photographer, make up, style and coordinate all process"
+                )}
               </li>
-              <li>{t('Ideas for graphic design: banners, newsletters, sales, new collections, presentation')}</li>
-              <li>{t('Managing team: photographer, graphic designer, models, smm department, brand manager')}</li>
-              <li>{t('Working with fashion bloggers, opinion makers, magazines and online channels')}</li>
-              <li>{t('Making research and hire team, testing, making test task')}</li>
+              <li>
+                {t(
+                  "Ideas for graphic design: banners, newsletters, sales, new collections, presentation"
+                )}
+              </li>
+              <li>
+                {t(
+                  "Managing team: photographer, graphic designer, models, smm department, brand manager"
+                )}
+              </li>
+              <li>
+                {t(
+                  "Working with fashion bloggers, opinion makers, magazines and online channels"
+                )}
+              </li>
+              <li>
+                {t("Making research and hire team, testing, making test task")}
+              </li>
             </ul>
           </div>
         </div>
       </section>
     </div>
-  )
+  );
 }
 
 export default Experience;
--- components/skills.js
+++ components/skills.js
@@ -1,86 +1,114 @@
-import React from 'react';
-import {useTranslation} from "react-i18next";
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { DiReact } from "react-icons/di";
+import { DiHtml5 } from "react-icons/di";
+import { DiCss3 } from "react-icons/di";
+import { DiJavascript1 } from "react-icons/di";
+import { DiPhp } from "react-icons/di";
+import { DiMagento } from "react-icons/di";
+import { FaGitAlt } from "react-icons/fa";
+import { FiFigma } from "react-icons/fi";
 
 function Skills() {
-  const {t} = useTranslation();
+  const { t } = useTranslation();
   return (
     <div className="skills-section">
       <section className="skills-container container">
-        <h1 className="skills-title">{t('Professional Skills')}</h1>
+        <h1 className="skills-title">{t("Professional Skills")}</h1>
         <div className="skills-content">
           <div className="skills-icons">
-            <div className="skills-icon">
-              <i className="fab fa-html5"/>
+            <div className="skills-icon-wrap">
+              <DiHtml5 className="skills-icon" />
               <span>HTML5</span>
             </div>
-            <div className="skills-icon">
-              <i className="fab fa-css3-alt"/>
+            <div className="skills-icon-wrap">
+              <DiCss3 className="skills-icon" />
               <span>CSS3</span>
             </div>
-            <div className="skills-icon">
-              <i className="icon-js"/>
+            <div className="skills-icon-wrap">
+              <DiJavascript1 className="skills-icon" />
               <span>JavaScript</span>
             </div>
-            <div className="skills-icon">
-              <i className="fab fa-react"/>
+            <div className="skills-icon-wrap">
+              <DiReact className="skills-icon" />
               <span>React JS</span>
             </div>
-            <div className="skills-icon">
-              <i className="fab fa-php"/>
+            <div className="skills-icon-wrap">
+              <DiPhp className="skills-icon" />
               <span>PHP</span>
             </div>
-            <div className="skills-icon">
-              <i className="fas fa-database"/>
+            <div className="skills-icon-wrap">
+              <i className="fas fa-database" />
               <span>SQL</span>
             </div>
-            <div className="skills-icon">
-              <i className="fab fa-git"/>
+            <div className="skills-icon-wrap">
+              <FaGitAlt className="skills-icon" />
               <span>Git</span>
             </div>
-            <div className="skills-icon">
-              <i className="fab fa-magento"/>
+            <div className="skills-icon-wrap">
+              <DiMagento className="skills-icon" />
               <span>Magento</span>
             </div>
-            <div className="skills-icon">
-              <i className="icon-ps"/>
+            <div className="skills-icon-wrap">
+              <FiFigma className="skills-icon" />
+              <span>Figma</span>
+            </div>
+            <div className="skills-icon-wrap">
+              <i className="icon-ps" />
               <span>Adobe Photoshop</span>
             </div>
-            <div className="skills-icon">
-              <i className="icon-ai"/>
+            <div className="skills-icon-wrap">
+              <i className="icon-ai" />
               <span>Adobe Illustrator</span>
             </div>
-            <div className="skills-icon">
-              <i className="icon-id"/>
-              <span>Adobe InDesign</span>
-            </div>
-            <div className="skills-icon">
-              <i className="fal fa-window"/>
+            <div className="skills-icon-wrap">
+              <i className="fal fa-window" />
               <span>SEO</span>
             </div>
           </div>
 
           <div className="skills-text">
             <section className="skills-text-section">
-              <h3 className="skills-text-title">{t('HTML5, CSS3 and Responsive/Mobile Web')}</h3>
-              <p className="skills-text-desc">{t('Design and build user interfaces for touch and mobile devices using: HTML5, CCS3 animations, CSS media queries, Flexbox, Figma, Adobe Photoshop, Adobe Illustrator, Adobe InDesign.')}</p>
+              <h3 className="skills-text-title">
+                {t("HTML5, CSS3 and Responsive/Mobile Web")}
+              </h3>
+              <p className="skills-text-desc">
+                {t(
+                  "Design and build user interfaces for touch and mobile devices using: HTML5, CCS3 animations, CSS media queries, Chakra UI, Tailwind CSS, Emotion, Styled Components, Flexbox, Grid, Figma, Adobe Photoshop, Adobe Illustrator."
+                )}
+              </p>
             </section>
             <section className="skills-text-section">
-              <h3 className="skills-text-title">{t('React, JavaScript and Single Page Applications (SPA)')}</h3>
-              <p className="skills-text-desc">{t('Experience in creating applications using Vanilla JS, ES6, React (Functional and Class Components, Hooks), JSX, React Router, Virtual DOM, AJAX, Fetch, NPM manager and WebSockets.')}</p>
+              <h3 className="skills-text-title">
+                {t("React, JavaScript and Single Page Applications (SPA)")}
+              </h3>
+              <p className="skills-text-desc">
+                {t(
+                  "Experience in creating applications using  React (Functional and Class Components, Hooks), Next.js, Vanilla JS, ES6, AJAX, Fetch, Redux Toolkit, NPM manager and WebSockets."
+                )}
+              </p>
             </section>
             <section className="skills-text-section">
-              <h3 className="skills-text-title">{t('Magento & PHP')}</h3>
-              <p className="skills-text-desc">{t('Experience in creating Magento based online stores with React based Frontends. Knowledge of Object Oriented Programming (OOP), SQL and MVC.')}</p>
+              <h3 className="skills-text-title">{t("Magento & PHP")}</h3>
+              <p className="skills-text-desc">
+                {t(
+                  "Experience in creating Magento based online stores with React based Frontends. Knowledge of Object Oriented Programming (OOP), SQL and MVC."
+                )}
+              </p>
             </section>
             <section className="skills-text-section">
-              <h3 className="skills-text-title">{t('SEO optimization')}</h3>
-              <p className="skills-text-desc">{t('Semantics of HTML structure, image and page optimization, Google Page Speed. Optimizing SEO for Google and Yandex searching engines.')}</p>
+              <h3 className="skills-text-title">{t("SEO optimization")}</h3>
+              <p className="skills-text-desc">
+                {t(
+                  "Semantics of HTML structure, image and page optimization, Google Page Speed. Optimizing SEO for Google and Yandex searching engines."
+                )}
+              </p>
             </section>
           </div>
         </div>
       </section>
     </div>
-  )
+  );
 }
 
 export default Skills;
